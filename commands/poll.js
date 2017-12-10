99c6761f4706a9f2ed7342b46edc2a7cf564c9cb
Ryan <rchinerman@gmail.com>
2017-12-10 13:30:23 -0800

    add reactions to poll automatically

--- commands/poll.js
+++ commands/poll.js
@@ -7,10 +7,15 @@ module.exports.run = async (bot, message, args) => {
         return entry.replace(/"/g,"");
       });
       let questions = noQuotes.slice(1).map((entry, i) => {
-        return {"name": emojis[i+1] + " " + entry, "value": "\u200B", inline: true}
+        let letter = String.fromCharCode(65 + i);        
+        return {"name": emojis[letter] + " " + entry, "value": "\u200B", inline: true}
       })
+      if (questions.length < 2){
+        message.channel.send("Please give at least two options.")
+        return;
+      }
       if(questions.length > 9){
-        message.channel.send("No more than 9 answers");
+        message.channel.send("Please give no more than 9 options.");
         return;
       }
       message.channel.send({embed: {
@@ -18,7 +23,16 @@ module.exports.run = async (bot, message, args) => {
         title: noQuotes[0],
         description: "Asked by " + message.author, 
         fields: questions     
-      }});
+      }}).then((msg)=>{
+        questions.forEach((e, i) => {
+          setTimeout(() => {            
+            let letter = String.fromCharCode(65 + i);
+            msg.react(emojis[letter]);   
+          }, i * 500);
+        })
+      }).catch((e) => {
+        console.log(e.stack);
+      })      
   } catch(err){
     console.log(err.stack);
   }
@@ -26,6 +40,6 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: "poll",
-  usage: ".poll \"Question\" \"Option 1\" \"Option 2\" etc. Quotes are required.",
+  usage: ".poll \"Question\" \"Option 1\" \"Option 2\"",
   about: "Prints out a short message describing each command."
 }
