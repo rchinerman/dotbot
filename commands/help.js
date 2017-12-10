d18feceec781cc5150aeaf09b0db818991f67f84
Ryan <rchinerman@gmail.com>
2017-12-10 01:44:32 -0800

    turn command help into a rich embed

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,20 @@
+const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
   try{
+    let fields = [];
     fs.readdir("./commands", (err, files) => {    
       let jsfiles = files.filter(f => f.split(".").pop() === "js");  
       jsfiles.forEach((f) => {
         let props = require(`./${f}`);
-        message.channel.send(`\`${props.help.name}: ${props.help.about}`);
+        fields.push({"name": `\`${props.help.name}`, "value": `${props.help.about}`});
       });
+      message.channel.send({embed: {
+        color: 3447003,
+        title: "Commands",      
+        fields: fields
+      }});
     });
   } catch(err){
     console.log(err.stack);
