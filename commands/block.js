d66b773be7410f1faa687d3e7b71d2052e2a3e83
Ryan <rchinerman@gmail.com>
2017-12-10 01:19:57 -0800

    ensure only allowed characters are printed in block function

--- commands/block.js
+++ commands/block.js
@@ -8,7 +8,13 @@ module.exports.run = async (bot, message, args) => {
       content += word + " "
     })
     content.split('').forEach((letter) => {
-      reply += emojis[letter.toUpperCase()]+' ';
+      let code = letter.charCodeAt();
+      if ((code > 47 && code < 58) || // numeric (0-9)
+          (code > 64 && code < 91) || // upper alpha (A-Z)
+          (code > 96 && code < 123) || // lower alpha (a-z)
+          (code === 32)) { // space
+            reply += emojis[letter.toUpperCase()]+' ';    
+      }
     })
     message.channel.send(`${reply}`);
   } catch(err){
