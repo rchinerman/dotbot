f525f1092b07a4e1982c55789a27277c149c7195
Ryan <rchinerman@gmail.com>
2017-12-10 00:53:11 -0800

    add puns

---
+++ commands/pun.js
@@ -0,0 +1,15 @@
+const jokes = require("../resources/jokes.json");
+
+module.exports.run = async (bot, message, args) => {
+  try{
+    let number = Math.floor(Math.random() * jokes.length);
+    message.channel.send(jokes[number]);
+  } catch(err){
+    console.log(err.stack);
+  }
+}
+
+module.exports.help = {
+  name: "pun",
+  about: "Prints out a short joke."
+}
