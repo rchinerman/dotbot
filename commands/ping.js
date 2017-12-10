6c776b427f44194e6719d6f788526fb2fd9f3232
Ryan <rchinerman@gmail.com>
2017-12-09 20:53:58 -0800

    add latency to pong return

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports.run = async (bot, message, args) => {
   try{
-    message.channel.send("pong");
-  } catch(e){
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+    } catch(e){
     console.log(e.stack);
   }
 }
